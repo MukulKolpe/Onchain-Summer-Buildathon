baec939f1423551da095d3a954d1245db1aae2bb
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-06-30 03:26:42 +0530

    Integrated smart wallet

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,49 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+"use client";
 
-const inter = Inter({ subsets: ["latin"] });
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { Button } from "@chakra-ui/react";
 
-export default function Home() {
+function Home() {
+  const account = useAccount();
+  const { connectors, connect, status, error } = useConnect();
+  const { disconnect } = useDisconnect();
+
   return (
     <>
-     Home
+      <div>
+        <h2>Account</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          addresses: {JSON.stringify(account.addresses)}
+          <br />
+          chainId: {account.chainId}
+        </div>
+
+        {account.status === "connected" && (
+          <Button type="button" onClick={() => disconnect()}>
+            Disconnect
+          </Button>
+        )}
+      </div>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <Button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </Button>
+        ))}
+        <div>{status}</div>
+        <div>{error?.message}</div>
+      </div>
     </>
   );
 }
+
+export default Home;
